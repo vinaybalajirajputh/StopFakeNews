a6da91c313de9bb934b747e128aae603afceb058
Vinay Balaji Rajputh <vinayrajputh@gmail.com>
2020-04-25 15:52:10 +0200

    Test commit

---
+++ src/chromeapp/frontend/background.js
@@ -0,0 +1,13 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("STOP FAKE NEWS CHROME EXTENSION INTIALIZED !!!");
+  });
+});
+
+
